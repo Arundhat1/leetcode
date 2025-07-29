2a1c4b5c0f6307ef361c5db5ed840f596fce8196
Arundhat1 <149229660+Arundhat1@users.noreply.github.com>
2025-07-29 22:01:11 +0530

    Create SmallestSubarraywithMaximumbitwiseor.py
    Started with brute-force O(n²), realized OR is monotonic → optimized using bit tracking from the end.
     Used reverse traversal and bit positions to compute minimum subarray length.
     Learned to spot properties like monotonic operations to reduce complexity.

---
+++ DailyQuestions/SmallestSubarraywithMaximumbitwiseor.py
@@ -0,0 +1,23 @@
+class Solution:
+    def smallestSubarrays(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        bitLastSeen = [-1] * 32
+        answer = [0] * n
+
+        for i in reversed(range(n)):
+            num = nums[i]
+            for b in range(32):
+                if (num >> b) & 1:
+                    bitLastSeen[b] = i
+
+            maxPos = i
+            for b in range(32):
+                if bitLastSeen[b] != -1:
+                    maxPos = max(maxPos, bitLastSeen[b])
+
+            answer[i] = maxPos - i + 1
+
+        return answer
+
+
+
