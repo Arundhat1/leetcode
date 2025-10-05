769372734826882a0cfd7fa3de5aa7f05a252381
Arundhat1 <149229660+Arundhat1@users.noreply.github.com>
2025-10-05 13:34:11 +0530

    Create Leetcode0237.py

---
+++ problems/Leetcode0237.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        Next  = node.next
+        value = Next.val
+        node.val = Next.val
+        node.next = Next.next
